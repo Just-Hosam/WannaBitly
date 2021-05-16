94f149db4495600d7cf249cad4eacf18d19a4a31
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-16 10:34:21 -0600

    database users and urls seeds

---
+++ backend/db/seeds/01_users_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, email, password) VALUES
+('Hosam', 'Dahrooge', 'hosam@test.com', 'test'),
+('Firas', 'Dahrooge', 'firas@test.com', 'test'),
+('Test', 'Dahrooge', 'test@test.com', 'test');
---
+++ backend/db/seeds/02_urls_seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO urls (user_id, short_url, long_url) VALUES
+(1, 'i am a short url', 'super longgg');
