7b373168c1f8f231fcdf63f4a1b6a86012737360
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-17 07:37:50 -0600

    Add more url seeds

--- backend/db/seeds/02_urls_seeds.sql
+++ backend/db/seeds/02_urls_seeds.sql
@@ -1,2 +1,7 @@
 INSERT INTO urls (user_id, short_url, long_url) VALUES
-(1, 'i am a short url', 'super longgg');
+(1, 'ShortNum1', 'LongNum1'),
+(1, 'ShortNum2', 'LongNum2'),
+(1, 'ShortNum3', 'LongNum3'),
+(2, 'ShortNum1', 'LongNum1'),
+(2, 'ShortNum2', 'LongNum2'),
+(2, 'ShortNum3', 'LongNum3');
