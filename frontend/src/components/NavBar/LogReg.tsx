ad7725111a03681b8face363291fdcc7d5409b88
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-17 19:59:09 -0600

    Login and Registration buttons component

---
+++ frontend/src/components/NavBar/LogReg.tsx
@@ -0,0 +1,16 @@
+import Button from '@material-ui/core/Button';
+
+const LogReg = () => {
+	return (
+		<div id="log-reg">
+			<Button variant="contained" className="nav-btn">
+				Login
+			</Button>
+			<Button variant="contained" className="nav-btn">
+				Register
+			</Button>
+		</div>
+	);
+};
+
+export default LogReg;
