935355051e0cacb7709b788bbab8618f1609058a
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-23 19:41:02 -0600

    Add clicks queries

---
+++ backend/db/queries/click-queries.ts
@@ -0,0 +1,50 @@
+const db = require('../../lib/db.js');
+
+interface Click {
+	id: number;
+	url_id: number;
+	click_timestamp: Date;
+}
+
+const getClicks = (urlId: number): Promise<Click[]> => {
+	const query = `
+	SELECT *
+	FROM clicks
+	WHERE url_id = $1
+	ORDER BY id DESC;`;
+	const values = [urlId];
+
+	return db
+		.query(query, values)
+		.then(({ rows }: { rows: Click[] }) => rows)
+		.catch((err: Error) => console.log(`Error at clicks queries 'getClicks'`, err));
+};
+
+const getClick = (urlId: number, clickId: number): Promise<Click> => {
+	const query = `
+	SELECT *
+	FROM clicks
+	WHERE url_id = $1
+	AND id = $2;`;
+	const values = [urlId, clickId];
+
+	return db
+		.query(query, values)
+		.then(({ rows }: { rows: Click[] }) => rows[0])
+		.catch((err: Error) => console.log(`Error at clicks queries 'getClick'`, err));
+};
+
+const addClick = (urlId: number, clickTimestamp: Date): Promise<Click> => {
+	const query = `
+	INSERT INTO clicks (url_id, click_timestamp)
+	VALUES ($1, $2)
+	RETURNING *;`;
+	const values = [urlId, clickTimestamp];
+
+	return db
+		.query(query, values)
+		.then(({ rows }: { rows: Click[] }) => rows[0])
+		.catch((err: Error) => console.log(`Error at clicks queries 'addClick'`, err));
+};
+
+export { getClicks, getClick, addClick };
---
+++ backend/dist/db/queries/click-queries.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.addClick = exports.getClick = exports.getClicks = void 0;
+const db = require('../../lib/db.js');
+const getClicks = (urlId) => {
+    const query = `
+	SELECT *
+	FROM clicks
+	WHERE url_id = $1
+	ORDER BY id DESC;`;
+    const values = [urlId];
+    return db
+        .query(query, values)
+        .then(({ rows }) => rows)
+        .catch((err) => console.log(`Error at clicks queries 'getClicks'`, err));
+};
+exports.getClicks = getClicks;
+const getClick = (urlId, clickId) => {
+    const query = `
+	SELECT *
+	FROM clicks
+	WHERE url_id = $1
+	AND id = $2;`;
+    const values = [urlId, clickId];
+    return db
+        .query(query, values)
+        .then(({ rows }) => rows[0])
+        .catch((err) => console.log(`Error at clicks queries 'getClick'`, err));
+};
+exports.getClick = getClick;
+const addClick = (urlId, clickTimestamp) => {
+    const query = `
+	INSERT INTO clicks (url_id, click_timestamp)
+	VALUES ($1, $2)
+	RETURNING *;`;
+    const values = [urlId, clickTimestamp];
+    return db
+        .query(query, values)
+        .then(({ rows }) => rows[0])
+        .catch((err) => console.log(`Error at clicks queries 'addClick'`, err));
+};
+exports.addClick = addClick;
