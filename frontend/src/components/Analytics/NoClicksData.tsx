d78e066147415fb577b60a694b5ca9823549be46
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-28 23:44:56 -0600

    No Clicks Data component

---
+++ frontend/src/components/Analytics/NoClicksData.tsx
@@ -0,0 +1,10 @@
+const NoClicksData = () => {
+	return (
+		<div id="no-clicks-data">
+			<h3>Not visited yet.</h3>
+			<p>Share your shortened URL on social media!</p>
+		</div>
+	);
+};
+
+export default NoClicksData;
