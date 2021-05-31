3e50f4b7bd2b24211160cb9bebd1bf87668edcac
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-31 12:20:00 -0600

    Remove seeds from database

--- backend/db/seeds/01_users_seeds.sql
+++ backend/db/seeds/01_users_seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (first_name, last_name, email) VALUES
-('Hosam', 'Dahrooge', 'hosam@test.com'),
-('Firas', 'Dahrooge', 'firas@test.com'),
-('Test', 'Dahrooge', 'test@test.com');
+-- INSERT INTO users (first_name, last_name, email) VALUES
+-- ('Hosam', 'Dahrooge', 'hosam@test.com'),
+-- ('Firas', 'Dahrooge', 'firas@test.com'),
+-- ('Test', 'Dahrooge', 'test@test.com');
