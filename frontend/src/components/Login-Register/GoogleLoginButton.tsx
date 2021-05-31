ed149e3a42dbc1e600253c12fdafdfa07a60c188
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-31 09:25:11 -0600

    Setup axios calls for login and registration Login user if email exists Register user if email doesn't exist

--- frontend/src/components/Login-Register/GoogleLoginButton.tsx
+++ frontend/src/components/Login-Register/GoogleLoginButton.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { GoogleLogin } from 'react-google-login';
 import refreshTokenSetup from '../../helpers/refreshTokenSetup';
 
@@ -5,15 +6,37 @@ import Button from '@material-ui/core/Button';
 
 const clientId = '850469791131-sr92fi9mga2ejm2ebhttiidb1o0mrnsq.apps.googleusercontent.com';
 
+interface User {
+	id: number;
+	first_name: string;
+	last_name: string;
+	email: string;
+}
+
 interface Props {
 	setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+	setUserData: React.Dispatch<React.SetStateAction<User>>;
 }
 
 const GoogleLoginButton = (props: Props) => {
 	const onSuccess = (res: any) => {
-		console.log('[Login Success] currentUser:', res.profileObj);
 		refreshTokenSetup(res);
 		props.setIsLoggedIn(true);
+
+		const userEmail = res.profileObj.email;
+		const userFirstName = res.profileObj.givenName;
+		const userLastName = res.profileObj.familyName;
+
+		axios
+			.post(`/login`, { userEmail })
+			.then((res) => {
+				if (res.data) return props.setUserData(res.data);
+				axios
+					.post('/register', { userEmail, userFirstName, userLastName })
+					.then((res) => props.setUserData(res.data))
+					.catch((err: Error) => console.log(err));
+			})
+			.catch((err: Error) => console.log(err));
 	};
 
 	const onFailure = (res: any) => {
