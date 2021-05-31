d91656323101d470a372c6f9b5d910d22160b7fd
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-05-31 12:18:40 -0600

    Conditionally render notLoggedIn component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,24 +1,25 @@
 import { useState } from 'react';
+import { useCookies } from 'react-cookie';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import NavBar from './NavBar/NavBar';
 import MainCard from './Main/MainCard';
 import AnalyticsCard from './Analytics/AnalyticsCard';
+import NotLoggedIn from './Main/NotLoggedIn';
 
 function App() {
+	const [cookies] = useCookies(['userId']);
 	const [analyticsId, setAnalyticsId] = useState(0);
+	const isLoggedIn = cookies.userId ? true : false;
 
 	return (
 		<div className="App">
 			<NavBar />
+			{!isLoggedIn && <NotLoggedIn />}
 			<Router>
 				<Switch>
-					<Route path="/analytics">
-						<AnalyticsCard analyticsId={analyticsId} />
-					</Route>
-					<Route path="/">
-						<MainCard setAnalyticsId={setAnalyticsId} />
-					</Route>
+					<Route path="/analytics">{isLoggedIn && <AnalyticsCard analyticsId={analyticsId} />}</Route>
+					<Route path="/">{isLoggedIn && <MainCard setAnalyticsId={setAnalyticsId} />}</Route>
 				</Switch>
 			</Router>
 		</div>
