84566681f4159dd412d5ec8e6cf2cbf34e0eb543
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-02 12:21:49 -0600

    Setup netlify frontend deployment

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './components/App';
+
+if (process.env.REACT_APP_API_BASE_URL) {
+	axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
