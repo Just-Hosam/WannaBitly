b6725523221edaa9914e283aa9b3174e2ff94e92
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-04 16:34:43 -0600

    NewUserInstruction component

---
+++ frontend/src/components/Main/NewUserInstructions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewUserInstructions = () => {
+	return (
+		<div id="new-user-instructions">
+			<h3>Let's get started</h3>
+			<p>Paste a URL then give it a description!</p>
+		</div>
+	);
+};
+
+export default NewUserInstructions;
