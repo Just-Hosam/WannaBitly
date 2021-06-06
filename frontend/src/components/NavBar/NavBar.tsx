12bf9fcf8876d2939edc0e9916e6b56823fc352c
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-06 10:23:20 -0600

    Remove login button from NavBar

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,5 @@
 import { useCookies } from 'react-cookie';
 
-import LogReg from './LogReg';
 import UserDetails from './UserDetails';
 
 const NavBar = () => {
@@ -10,10 +9,7 @@ const NavBar = () => {
 	return (
 		<div id="navbar">
 			<h1>WannaBitly</h1>
-			<div id="nav-details">
-				{!isLoggedIn && <LogReg />}
-				{isLoggedIn && <UserDetails />}
-			</div>
+			{isLoggedIn && <UserDetails />}
 		</div>
 	);
 };
