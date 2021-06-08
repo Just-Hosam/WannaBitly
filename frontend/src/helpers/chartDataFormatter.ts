e99b55e10b4a6dbcef92681e26da4077f875b9b1
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-06-08 09:03:11 -0600

    Fix intentional bug introduced on last deploy

--- frontend/src/helpers/chartDataFormatter.ts
+++ frontend/src/helpers/chartDataFormatter.ts
@@ -16,7 +16,7 @@ const chartDataFormatter = (clicksData: Click[], isMobile: boolean): DataPoint[]
 	for (const click of clicksData) {
 		const localDate = new Date(Number(click.timestamp)).toLocaleDateString();
 		const dateArr = localDate.split('/');
-		const monthPart = monthNameGen(Number(isMobile ? dateArr[0] : dateArr[0]));
+		const monthPart = monthNameGen(Number(isMobile ? dateArr[1] : dateArr[0]));
 		const yearPart = dateArr[2][2] + dateArr[2][3];
 		const month = `${monthPart} ${yearPart}`;
 
